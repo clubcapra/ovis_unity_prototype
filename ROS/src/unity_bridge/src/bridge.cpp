7915d09cb0e3a75bb131c21681bb8755c3d0861e
Marc-Olivier <marcolivier.belisle.1@gmail.com>
2022-03-14 17:50:47 -0400

    Refactor the cpp node with the new messages

--- ROS/src/unity_bridge/src/bridge.cpp
+++ ROS/src/unity_bridge/src/bridge.cpp
@@ -1,71 +1,87 @@
 #include "ros/ros.h"
-#include "geometry_msgs/Pose.h"
-#include "ovis_msgs/OvisJoints.h"
+#include "ovis_msgs/OvisJointAngles.h"
+#include "ovis_msgs/OvisJointGoal.h"
+#include "ovis_msgs/OvisIKGoal.h"
+#include "ovis_msgs/HomeJoint.h"
 
-constexpr size_t JOINTS_COUNT = 7;
+constexpr size_t JOINTS_COUNT = 6;
 
-void targetCallback(const geometry_msgs::Pose::ConstPtr& msg)
+enum class ArmState { Idle, JointByJoint, IKGoal };
+
+ArmState armState;
+ovis_msgs::OvisJointGoal goal;
+
+std::vector<float> joint_angles;
+
+// void targetCallback(const geometry_msgs::Pose::ConstPtr& msg)
+// {
+//     ROS_INFO("I heard: [%e, %e, %e]", msg->position.x, msg->position.y, msg->position.z);
+// }
+
+void OvisJointGoalCallback(const ovis_msgs::OvisJointGoal::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%e, %e, %e]", msg->position.x, msg->position.y, msg->position.z);
+    ROS_INFO("I heard OvisJointGoalCallback: [%d, %e]", msg->joint_index, msg->joint_angle);
+
+    goal.joint_index = msg->joint_index;
+    goal.joint_angle = msg->joint_angle;
+
+    armState = ArmState::JointByJoint;
 }
 
-void simCallback(const ovis_msgs::OvisJoints::ConstPtr& msg)
+bool HomeJointCallback(ovis_msgs::HomeJointRequest &request, ovis_msgs::HomeJointResponse &response)
 {
-  ROS_INFO("I heard: [%e, %e, %e, %e, %e, %e, %e]", 
-    msg->joint_angles[0], 
-    msg->joint_angles[1], 
-    msg->joint_angles[2],
-    msg->joint_angles[3],
-    msg->joint_angles[4],
-    msg->joint_angles[5],
-    msg->joint_angles[6]);
+    for (size_t i = 0; i < JOINTS_COUNT; i++)
+    {
+        response.home_joint_positions.push_back(0);
+    }
+    response.home_joint_positions[1] = 180;
+
+    return true;
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "unity_bridge");
+    ros::init(argc, argv, "unity_bridge");
 
-  ros::NodeHandle n;
+    ros::NodeHandle n;
 
-  //Need this when controlling the arm from Unity
-  n.subscribe<ovis_msgs::OvisJoints>("sim_joints", 1000, simCallback);
+    ros::ServiceServer home_srv = n.advertiseService("ovis/home_joint_positions", HomeJointCallback);
 
-  //Need this when using the IK on the robot arm
-  n.subscribe<geometry_msgs::Pose>("ovis_target", 1000, targetCallback);
-  ros::Publisher joints_pub = n.advertise<ovis_msgs::OvisJoints>("ovis_joints", 1000);
+    ros::Publisher joints_pub = n.advertise<ovis_msgs::OvisJointAngles>("ovis/joint_angles", 1);
+    
+    //JointByJoint topic
+    ros::Subscriber jgoal_sub = n.subscribe<ovis_msgs::OvisJointGoal>("ovis/joint_goal", 100, OvisJointGoalCallback);
 
-  ros::Rate loop_rate(10);
-
-  int count = 0;
-  while (ros::ok())
-  {
-    ovis_msgs::OvisJoints msg;
-    msg.header.seq = count;
-    msg.header.stamp = ros::Time::now();
-    msg.header.frame_id = "ovis";
-
-    std::vector<std::string> joint_names;
-    std::vector<float> joint_angles;
-
+    // IKGoal topic
+    // n.subscribe<geometry_msgs::Pose>("ovis_target", 1000, targetCallback);
+    
+    armState = ArmState::Idle;
     for (size_t i = 0; i < JOINTS_COUNT; i++)
     {
-      joint_names.push_back("name" + std::to_string(i));
-      joint_angles.push_back(0);
+        joint_angles.push_back(0);
+    }
+    joint_angles[1] = 180;
+
+    ros::Rate loop_rate(10);
+
+    while (ros::ok())
+    {
+        //Check for new messages
+        ros::spinOnce();
+
+        switch (armState)
+        {
+        case ArmState::JointByJoint:
+            joint_angles[goal.joint_index] = goal.joint_angle;
+            break;
+        }
+
+        ovis_msgs::OvisJointAngles joint_msg;
+        joint_msg.joint_angles = joint_angles;
+        joints_pub.publish(joint_msg);
+
+        loop_rate.sleep();
     }
 
-    //For test purpose only
-    joint_angles[0] = count;
-
-    msg.joint_names = joint_names;
-    msg.joint_angles = joint_angles;
-
-    joints_pub.publish(msg);
-
-    ros::spinOnce();
-
-    loop_rate.sleep();
-    count++;
-  }
-
-  return 0;
+    return 0;
 }
